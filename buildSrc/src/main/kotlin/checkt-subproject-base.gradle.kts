548636775da410382da2f709228490ae256a0817
Dawid Wachura <dawid.wachura@gmail.com>
2023-06-22 21:12:43 +0200

    temp mvn publish

--- buildSrc/src/main/kotlin/checkt-subproject-base.gradle.kts
+++ buildSrc/src/main/kotlin/checkt-subproject-base.gradle.kts
@@ -1,8 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm")
     `maven-publish` apply false
+//    signing apply false
 //    id("org.jmailen.kotlinter")
 }
 
@@ -11,11 +10,14 @@ version = rootProject.version
 repositories {
     mavenLocal()
     mavenCentral()
-}
-
-dependencies {
-    implementation(Dependencies.`kotlin-logging`)
-    testRuntimeOnly(Dependencies.`logback-classic`)
+    maven {
+        url = uri(
+            when {
+                isSnapshot() -> "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                else -> "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            }
+        )
+    }
 }
 
 findProperty("checkt.testing.disabled") ?: run {
@@ -30,18 +32,39 @@ findProperty("checkt.testing.disabled") ?: run {
 }
 
 findProperty("checkt.maven.publishing.disabled") ?: run {
-    apply { plugin("org.gradle.maven-publish") }
+    apply { plugin(MavenPublishPlugin::class) }
+    apply { plugin(SigningPlugin::class) }
 
     val artifactName = artifactName()
 
     publishing {
         publications {
             create<MavenPublication>("maven") {
+                groupId = "${rootProject.group}"
                 artifactId = artifactName
                 from(components["java"])
             }
         }
+
+        repositories {
+            maven {
+                url = uri(
+                    when {
+                        isSnapshot() -> "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                        else -> "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                    }
+                )
+                credentials {
+                    username = ""
+                    password = ""
+                }
+            }
+        }
     }
+
+//    signing {
+//        sign(publishing.publications["maven"])
+//    }
 
     tasks.jar {
         archiveBaseName.set(artifactName)
